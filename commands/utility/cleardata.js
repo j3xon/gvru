34ee797fdcef47df52ac47418e5c927e9cc9bc6b
Jax_on <roleplaysboys@gmail.com>
2025-06-22 20:14:39 +0200

    Update cleardata.js

--- commands/utility/cleardata.js
+++ commands/utility/cleardata.js
@@ -15,22 +15,18 @@ module.exports = {
 
   async execute(interaction) {
     try {
-      // Check if the user has the required role (Replace with your role ID)
-      const requiredRoleId = '1304801318216532070'; // Replace with the actual role ID
+      const requiredRoleId = '1304801318216532070';
       if (!interaction.member.roles.cache.has(requiredRoleId)) {
         return interaction.reply({ content: 'You do not have permission to use this command. You must have the appropriate admin role.', ephemeral: true });
       }
 
-      // Get the user ID from the command
       const userId = interaction.options.getString('userid');
 
-      // Delete data from the relevant collections
       const verificationData = await Verification.findOneAndDelete({ discordId: userId });
       const vehicleData = await Vehicle.deleteMany({ userId });
       const ticketData = await Ticket.deleteMany({ userId });
       const licenseData = await License.findOneAndDelete({ userId });
 
-      // Check if any data was deleted and reply accordingly
       if (verificationData || vehicleData.deletedCount > 0 || ticketData.deletedCount > 0 || licenseData) {
         await interaction.reply({ content: `Successfully cleared all data for user <@${userId}>.`, ephemeral: true });
       } else {
