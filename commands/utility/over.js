08ac1a426d4c94921ea978e4536e484d905af4dd
Jax_on <roleplaysboys@gmail.com>
2025-06-22 20:16:33 +0200

    Update over.js

--- commands/utility/over.js
+++ commands/utility/over.js
@@ -7,14 +7,12 @@ module.exports = {
         .setDescription('Ends the session'),
 
     async execute(interaction) {
-        // Check if the user has the required role
         const requiredRoleId = '1343059207900889140';
         if (!interaction.member.roles.cache.has(requiredRoleId)) {
             return await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
         }
 
         try {
-            // Create the session over embed
             const embed = new EmbedBuilder()
                 .setTitle('Over')
                 .setDescription(`> <@${interaction.user.id}> has ended their roleplay session. We hope you enjoyed the session. Feel free to rate the session by filling out the form provided below.
@@ -23,10 +21,9 @@ module.exports = {
                 .setImage("https://cdn.discordapp.com/attachments/1304716049794469959/1305215245152948257/session_3.png?ex=673237ec&is=6730e66c&hm=ba2c7b45fef3a4bb4f5935349cac8933e48bfe23fab71bf5e551ea155ef43e29&")
                 .setColor('#8accff');
 
-            // Create the session log embed
-            const startTime = "Start Time"; // Replace with actual start time
-            const endTime = "End Time"; // Replace with actual end time
-            const date = new Date().toLocaleDateString(); // Current date
+            const startTime = "Start Time"; 
+            const endTime = "End Time";
+            const date = new Date().toLocaleDateString();
 
             const newEmbed = new EmbedBuilder()
                 .setTitle("Command Executed")
@@ -39,26 +36,21 @@ module.exports = {
                 )
                 .setColor('#8accff');
 
-            // Create the session rating button
             const button = new ButtonBuilder()
                 .setCustomId('session_rating_button')
                 .setLabel('Session Form')
                 .setStyle(ButtonStyle.Primary);
 
-            // Create an ActionRow for the button
             const row = new ActionRowBuilder().addComponents(button);
 
-            // Send the embed to the current channel, mentioning everyone
             await interaction.channel.send({
                 embeds: [embed],
                 components: [row]
             });
 
-            // Send session log to the specified log channel
             const logChannel = await interaction.client.channels.fetch('1304564515844919296');
             await logChannel.send({ embeds: [newEmbed] });
 
-            // Reply to the interaction to confirm the command was executed
             await interaction.reply({ content: 'Command executed successfully!', ephemeral: true });
         } catch (error) {
             console.error('Error sending messages:', error);
