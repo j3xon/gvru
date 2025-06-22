4af900f503b12a26d0f3304562b4af733b9998dd
Jax_on <roleplaysboys@gmail.com>
2025-06-22 20:13:51 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,9 +4,8 @@ module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
-        console.log(`${client.user.tag} online`); // Logs when the bot is online
+        console.log(`${client.user.tag} online`);
 
-        // Set the bot's activity
         client.user.setActivity("discord.gg/gvunity", { type: ActivityType.Watching });
         client.user.setStatus("online");
     },
